984a257f8236050663ca9b441abe3c077b4723a2
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-22 08:25:52 -0800

    make sure password is more secure when signing up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :events
+
+  validate :password_complexity
+
+  def password_complexity
+    if password.present? and not password.match(/\A(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/)
+      errors.add :password, "must contain at least 6 characters, a number, a lowercase letter, uppercase letter, a symbol"
+    end
+  end
 end
