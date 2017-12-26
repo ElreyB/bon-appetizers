448efb9d6cdfde0c762f1f59fc2ee97c65c3fb85
Elrey Belmonti <elbelmonti@gmail.com>
2017-12-26 09:24:27 -0800

    add regex notice

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,15 @@ class User < ApplicationRecord
   validate :password_complexity
 
   def password_complexity
+    
+    # PASSWORD_FORMAT = /\A
+    #   (?=.{8,})          # Must contain 8 or more characters
+    #   (?=.*\d)           # Must contain a digit
+    #   (?=.*[a-z])        # Must contain a lower case character
+    #   (?=.*[A-Z])        # Must contain an upper case character
+    #   (?=.*[[:^alnum:]]) # Must contain a symbol
+    # /x
+
     if password.present? and not password.match(/\A(?=.{6,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[[:^alnum:]])/)
       errors.add :password, "must contain at least 6 characters, a number, a lowercase letter, uppercase letter, a symbol"
     end
