69fe12bb6390d351288dd3875d7acfeee6bb31ce
Elrey Belmonti <elbelmonti@gmail.com>
2018-03-22 00:38:58 -0700

    add jquery to show style form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,12 +27,11 @@ $(document).ready(function () {
     interval: 3000
   });
 
-  function OnSelectionChange() {
-    const selectedOption = this.options[this.selectedIndex].innerText;
-    if (selectedOption === "HORS D’OEUVRES") {
+  function showForm(formOptionText) {
+    if (formOptionText === "HORS D’OEUVRES") {
       document.getElementById('bits_form').hidden = false;
       document.getElementById('family_form').hidden = true;
-    } else if ((selectedOption === "The Traditional Italian Dinner") || (selectedOption === "The Traditional Italian Dinner with Individually Plated Entrees")) {
+    } else if ((formOptionText === "The Traditional Italian Dinner") || (formOptionText === "The Traditional Italian Dinner with Individually Plated Entrees")) {
       document.getElementById('family_form').hidden = false;
       document.getElementById('bits_form').hidden = true;
     } else {
@@ -41,18 +40,12 @@ $(document).ready(function () {
     }
   }
 
-  const menuStyle = document.getElementById('menu_style_id');
-  if (menuStyle !== null) {
-    const menuStyleValue = menuStyle.options[menuStyle.selectedIndex].innerText;
-    if (menuStyleValue === "HORS D’OEUVRES") {
-      document.getElementById('bits_form').hidden = false;
-      document.getElementById('family_form').hidden = true;
-    } else if ((menuStyleValue === "The Traditional Italian Dinner") || (menuStyleValue === "The Traditional Italian Dinner with Individually Plated Entrees")) {
-      document.getElementById('family_form').hidden = false;
-      document.getElementById('bits_form').hidden = true;
-    } else {
-      document.getElementById('family_form').hidden = true;
-      document.getElementById('bits_form').hidden = true;
-    }
-  }
+  const selectedOption = $('#menu_style_id option:selected').text();
+  showForm(selectedOption);
+
+  $('#menu_style_id').change(function () {
+    const changedOption = $('#menu_style_id option:selected').text();
+    showForm(changedOption);
+  })
+
 });
