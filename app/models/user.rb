f1f54f92b0d32be2ecbb1d7592ae3c46c131b217
Elrey Belmonti <elbelmonti@gmail.com>
2018-03-25 10:25:14 -0700

    refactor devise error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
   def password_complexity
 
     # PASSWORD_FORMAT = /\A
-    #   (?=.{8,})          # Must contain 8 or more characters
+    #   (?=.{6,})          # Must contain 6 or more characters
     #   (?=.*\d)           # Must contain a digit
     #   (?=.*[a-z])        # Must contain a lower case character
     #   (?=.*[A-Z])        # Must contain an upper case character
