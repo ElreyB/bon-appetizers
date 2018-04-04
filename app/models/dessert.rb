b6402d4ad0bbec6e75e2b4d1b2d4ca985bfd431e
Elrey Belmonti <elbelmonti@gmail.com>
2018-04-04 09:09:33 -0700

    unify menu item index pages

--- app/models/dessert.rb
+++ app/models/dessert.rb
@@ -4,7 +4,7 @@ class Dessert < ActiveRecord::Base
   validates :name, uniqueness: true
   validates :name, :description, presence: true
 
-  has_attached_file :image, styles: { large: "600X600>", medium: "300x300>", thumb: "100x100#" }, default_url: ":style/missing.jpg"
+  has_attached_file :image, styles: { large: "600X600>", medium: "300x300>", small: "250x250", thumb: "100x100#" }, default_url: ":style/missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def self.pick_of_the_day
--- app/models/style.rb
+++ app/models/style.rb
@@ -4,6 +4,6 @@ class Style < ApplicationRecord
   validates :name, :description, :price, :pairing, presence: true
   scope :by_name, -> {order(name: :ASC)}
 
-  has_attached_file :image, styles: { large: "600X600>", medium: "300x300>", thumb: "100x100#" }, default_url: ":style/missing.jpg"
+  has_attached_file :image, styles: { large: "600X600>", medium: "300x300>", small: "250x250", thumb: "100x100#" }, default_url: ":style/missing.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
